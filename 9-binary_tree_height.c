cc455c8074da28a8065a737c3a76001b077fddc9
Ricardo Hincapie <hincapiet.ricardo@gmail.com>
2020-07-21 21:44:29 +0000

    Add tasks #10 to #13

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,6 +3,8 @@
 size_t binary_tree_height(const binary_tree_t *tree)
 {
     if(tree == NULL)
-		return(0);
+		  return(0);
+    if(tree->left == NULL && tree->right == NULL)
+      return(0);
     return(1 + (MAX(binary_tree_height(tree->left), binary_tree_height(tree->right))));
 }
